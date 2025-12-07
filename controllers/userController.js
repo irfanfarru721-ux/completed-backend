86c607d3eb7b9d51dcc9b4a1f83498d75a2a2a1b
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-07 15:48:23 +0530

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,45 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const exists = await User.findOne({ email });
+    if (exists)
+      return res.status(400).json({ message: "Email already exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashed,
+    });
+
+    res.json({ message: "User registered", user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid email" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ message: "Invalid password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({ message: "Login successful", token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
