ed4dd4fbae5dcb27b14d5d0894cd3da6e5aebd2d
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-12-07 13:04:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://overfrontadmin.onrender.com",
+      "https://frontend-admin-lhhq.onrender.com",
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
